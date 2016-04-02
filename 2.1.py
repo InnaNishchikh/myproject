d6cdea37656c43c1d48fc4136d3aa37a7c61f422
jekaaa <zhenja-g10@rambler.ru>
2016-04-02 15:32:17 +0500

    2.1

---
+++ 2.1.py
@@ -0,0 +1,29 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('',8000))
+s.listen(1)
+
+while (True):
+    connection,address = s.accept()
+    data = str(connection.recv(1024))
+    print(data)
+    path = data.split('\n')[0].split(' ')[1]
+    path = path[1:]
+    if (path == ''):
+        path == 'index.html'
+    file = open(path,'rb')
+    response = """\
+HTTP/1.1 200 OK
+Content-Type: text/html
+
+
+
+""" + str(file.read())
+    connection.send(response.encode())
+    
+connection.close()
+s.close()
+
+
